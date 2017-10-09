74388b0b8b29c9147d180bb336f67f43c1d87567
Vince Carey <stvjc@channing.harvard.edu>
2017-10-09 08:28:13 -0400

    add return statement to skirt warning

--- R/fastGrep.R
+++ R/fastGrep.R
@@ -3,6 +3,7 @@
 #' @param onto an ontologyIndex instance
 #' @param field the ontologyIndex component to be searched
 #' @param \dots passed to grep
+#' @return logical vector indicating vector or list elements where a match is found
 #' @examples
 #' cheb = getChebiOnto()
 #' ind = fastGrep("17-AAG", cheb, "synonym")
