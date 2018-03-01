4a4798505e96a18805b7c5e30b295384e7c2d199
Vince Carey <stvjc@channing.harvard.edu>
2018-03-01 10:40:10 -0500

    adding minicorpus for SRA annotation

--- R/data.R
+++ R/data.R
@@ -8,3 +8,32 @@
 #' data(allGOterms)
 #' head(allGOterms)
 "allGOterms"
+#' stopWords: vector of stop words from xpo6.com
+#' @docType data
+#' @format character vector
+#' @source \url{http://xpo6.com/list-of-english-stop-words/}
+#' @note "Stop words" are english words that are assumed to contribute limited 
+#' semantic value in the analysis of free text. 
+#' @examples
+#' data(stopWords)
+#' head(stopWords)
+"stopWords"
+#' minicorpus: a vector of annotation strings found in 'study title' of SRA metadata.
+#' @docType data
+#' @format character vector
+#' @source NCBI SRA
+#' @note arbitrarily chosen from titles of RNA-seq studies for taxon 9606
+#' @examples
+#' data(minicorpus)
+#' head(minicorpus)
+"minicorpus"
+#' humrna: a data.frame of SRA metadata related to RNA-seq in humans
+#' @docType data
+#' @format data.frame
+#' @source NCBI SRA
+#' @note arbitrarily chosen from RNA-seq studies for taxon 9606
+#' @examples
+#' data(humrna)
+#' names(humrna)
+#' head(humrna[,1:5])
+"humrna"
---
+++ R/dropStop.R
@@ -0,0 +1,21 @@
+#' dropStop is a utility for removing certain words from text data
+#' @param x character vector of strings to be cleaned
+#' @param drop character vector of words to scrub
+#' @param lower logical, if TRUE, x converted with \code{\link{tolower}}
+#' @param splitby character, used with strsplit to tokenize \code{x}
+#' @return a list with one element per input string, split by " ", with elements in \code{drop} removed
+#' @examples
+#' data(minicorpus)
+#' minicorpus[1:3]
+#' dropStop(minicorpus)[1:3]
+#' @export
+dropStop = function(x, drop, lower=TRUE, splitby=" ") {
+ if (missing(drop)) {
+    data(stopWords, package="ontoProc")
+    drop = stopWords
+    }
+ tx = force
+ if (lower) tx = tolower
+ spl = strsplit(tx(x), splitby)
+ lapply(spl, setdiff, drop)
+}
