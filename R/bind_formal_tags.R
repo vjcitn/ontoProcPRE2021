6f231079990831cde1d4ddbdba70901e1868eeba
Vince Carey <stvjc@channing.harvard.edu>
2020-01-31 14:58:54 -0500

    don't specialize bind_formal_tags and subset_descendants to SCE, simplify to SE

--- R/bind_formal_tags.R
+++ R/bind_formal_tags.R
@@ -1,20 +1,20 @@
-#' add mapping from informal to formal cell type tags to a SingleCellExperiment colData
-#' @param sce SingleCellExperiment instance
+#' add mapping from informal to formal cell type tags to a SummarizedExperiment colData
+#' @param sce SummarizedExperiment instance
 #' @param informal character(1) name of colData element with uncontrolled vocabulary
 #' @param tagmap data.frame with columns 'informal' and 'formal'
 #' @param force logical(1), defaults to FALSE; if TRUE, allows clobbering existing colData variable named "formal"
-#' @return SingleCellExperiment instance with a new colData column 'formal' giving
+#' @return SummarizedExperiment instance with a new colData column 'formal' giving
 #' the formal tags associated with each sample
 #' @note This function will fail if the value of `informal` is not among the
 #' colData variable names, or if "formal" is among the colData variable names.
 #' @export
-bind_formal_tags = function(sce, informal, tagmap, force=FALSE) {
-  cdn = names(colData(sce))
-  if (!(informal %in% cdn)) stop(sprintf("can't find %s in names(colData(sce))", informal))
+bind_formal_tags = function(se, informal, tagmap, force=FALSE) {
+  cdn = names(colData(se))
+  if (!(informal %in% cdn)) stop(sprintf("can't find %s in names(colData(se))", informal))
   if ("formal" %in% cdn && !force) stop("won't clobber existing colData variable 'formal', please rename or set force = TRUE")
   if (!(all(c("informal", "formal") %in% names(tagmap)))) stop("tagmap must include columns named 'formal' and 'informal'")
   mapv = tagmap[["formal"]]
   names(mapv) = tagmap[["informal"]]
-  colData(sce)$formal = mapv[sce[[informal]]]
-  sce
+  colData(se)$formal = mapv[se[[informal]]]
+  se
 }
--- R/subset_descendants.R
+++ R/subset_descendants.R
@@ -1,15 +1,15 @@
-#' subset a SingleCellExperiment to which ontology tags have been bound using 'bind_formal_tags',
+#' subset a SummarizedExperiment to which ontology tags have been bound using 'bind_formal_tags',
 #' obtaining the 'descendants' of the class of interest
 #' @importFrom ontologyIndex get_descendants
-#' @param sce SingleCellExperiment instance
+#' @param se SummarizedExperiment instance
 #' @param onto representation of an ontology using representation from ontologyIndex package
 #' @param class_name character(1) if 'class_tag' is missing, this will be grepped in onto[["name"]] to find class and its descendants
-#' @param class_tag character(1) used if given to identify "ontological descendants" of this term in sce 
-#' @return instance of SingleCellExperiment
+#' @param class_tag character(1) used if given to identify "ontological descendants" of this term in se 
+#' @return instance of SummarizedExperiment
 #' @export
-subset_descendants = function(sce, onto, class_name, class_tag) {
-# verify 'formal' exists in sce
- stopifnot("formal" %in% names(colData(sce)))
+subset_descendants = function(se, onto, class_name, class_tag) {
+# verify 'formal' exists in se
+ stopifnot("formal" %in% names(colData(se)))
 # find the class tag if not supplied
  if (missing(class_tag)) {
    inds = grep(class_name, onto[["name"]])
@@ -19,6 +19,6 @@ subset_descendants = function(sce, onto, class_name, class_tag) {
    class_tag = names(onto[["name"]][inds])
  }
  desc = get_descendants(onto, class_tag)
- sce[, which(sce$formal %in% desc)]
+ se[, which(se$formal %in% desc)]
 }
 
